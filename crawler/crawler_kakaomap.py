d8270d5bc61284af7075893daabc33c8fa3cd597
junsikkkk <114077810+junsikkkk@users.noreply.github.com>
2023-07-16 03:53:13 +0900

    Update crawler_kakaomap_main.py

--- crawler/crawler_kakaomap.py
+++ crawler/crawler_kakaomap.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[44]:
-
-
 import re
 import time
 import os
@@ -15,66 +9,9 @@ import csv
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
-#import googlemaps
 import pandas as pd
 headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
-
-
-# In[89]:
-
-
-def storeNamePrint() :
-    
-    time.sleep(0.2)
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    cafe_lists = soup.find_all(name='li', attrs={"class": "PlaceItem clickArea"})
-    #s = len(cafe_lists)
-    count = 1
-    for cafe in cafe_lists :
-        temp = []
-        cafe_name = cafe.find(name = 'a', attrs={"class" : "link_name"}).text
-        food_score = cafe.find(name = 'em', attrs={"data-id" : "scoreNum"}).text
-        score_num = cafe.find(name = 'a', attrs={"data-id" : "numberofscore"}).text
-        #review = soup.find(name = 'em', attrs={"class" : "num"})
-        reviewnum = cafe.find(name = 'em', attrs={"data-id" : "numberofreview"}).text
-        addr = cafe.find(name = 'p', attrs={"data-id" : "address"}).text
-        cate = cafe.find(name = 'span', attrs={"data-id" : "subcategory"}).text
-        open1 = cafe.find(name = 'a', attrs={"data-id" : "periodTxt"}).text
-        
-        #review = review[3:len(review)]
-        #review = int(re.sub(",","",review))
-        
-        temp.append(cafe_name)
-        temp.append(food_score)
-        temp.append(score_num)
-        #temp.append(review)
-        temp.append(reviewnum)
-        temp.append(addr)
-        temp.append(cate)
-        temp.append(open1)
-        
-        list.append(temp)
-    f = open(filename+'.csv','w',encoding='utf-8-sig',newline="")
-    writercsv=csv.writer(f)
-    header=['Name','food_Score','Score_num','Review','Addr','Category', 'OpenTime']
-    writercsv.writerow(header)
-    
-    for i in list:
-        writercsv.writerow(i)
-
-
-# In[64]:
-
-
-list = []
-
-
-# In[92]:
-
-
+################
 list = []
 url = "https://map.kakao.com"
 options = webdriver.ChromeOptions()
@@ -109,4 +46,3 @@ for i in range(0,35):
     except:
         break
 print('**크롤링완료**')
-
